86c75919adac3b7210a2770c1a506ffb977bb502
RuohengWeng <maplebyriver@gmail.com>
2023-03-27 23:31:18 +0100

    change favicon

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,21 @@
-const App = () => {
+import { useState } from "react"
+import "./css/styles.css";
+import LargeText from "./components/LargeText";
+
+function App () {
+  const [counter, setCounter] = useState(0)
+  const buttonText = "Click me";
+  const onClickHanlder = () => {
+    console.log(counter)
+    setCounter(counter + 1);
+  }
+
   return (
-    <div>
-        <h1>Images Gallery</h1>
+    <div className="App">
+        <button onClick={onClickHanlder}>{buttonText}</button>
+        <LargeText text={`Butoon was clicked ${counter} times`} />
     </div>
   );
 }
 
-export default App;
+export default App
---
+++ frontend/src/components/LargeText.js
@@ -0,0 +1,6 @@
+function LargeText ({text}) {
+    console.log("re-rendered once")
+    return <h1>{text}</h1>
+}
+
+export default LargeText
